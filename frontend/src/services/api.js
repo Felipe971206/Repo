d9439169979375cc0faa1f8d15935a6bedf6aea3
Felipe971206 <166779985+Felipe971206@users.noreply.github.com>
2024-04-12 23:32:55 -0500

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3001',  // Asegúrate de usar el puerto correcto donde se ejecuta tu backend
+    baseURL: 'http://localhost:3001', 
     headers: {
         'Content-Type': 'application/json'
     }
